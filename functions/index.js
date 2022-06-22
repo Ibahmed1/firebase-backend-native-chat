d1a4b6686e68c4c9770191c246f2f8aa2ff2b429
Ibahmed1 <ib.ahmed1@outlook.com>
2022-06-22 16:09:50 +0100

    addFriend api functionality now works when provided with two emails. Will not add if users are already friends

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const { getAuth } = require("firebase-admin/auth");
+const { getFirestore, Timestamp, FieldValue } = require("firebase-admin/firestore");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -11,6 +12,10 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://native-chat-cfcdc-default-rtdb.europe-west1.firebasedatabase.app",
 });
+
+const db = getFirestore();
+const messagesTable = db.collection("messages");
+const friendsTable = db.collection("friends");
 
 app.use(cors({ origin: true }));
 
@@ -23,9 +28,42 @@ app.get("/user", async (req, res) => {
     const user = await getAuth().getUserByEmail(email);
     return res.status(200).json(user);
   } catch (error) {
-    return res.status(500).json(error);
+    return res.status(500).json({ error: error });
   }
 });
+app.post("/friends", addFriend);
+app.get("/friends", getFriends);
+
+async function getFriends(req, res) {
+  const userEmail = req.query.email;
+  const user = (await getAuth().getUserByEmail(userEmail)).uid;
+}
+
+async function addFriend(req, res) {
+  const { userEmail, friendEmail } = req.body;
+  const userOneId = (await getAuth().getUserByEmail(userEmail)).uid;
+  const userTwoId = (await getAuth().getUserByEmail(friendEmail)).uid;
+  const friendsCheck1 = await friendsTable
+    .where("user_one_id", "==", userOneId)
+    .where("user_two_id", "==", userTwoId)
+    .get();
+  const friendsCheck2 = await friendsTable
+    .where("user_one_id", "==", userTwoId)
+    .where("user_two_id", "==", userOneId)
+    .get();
+  if (friendsCheck1.empty && friendsCheck2.empty)
+    try {
+      const res = await friendsTable.add({
+        created_at: Timestamp.now(),
+        user_one_id: userOneId,
+        user_two_id: userTwoId,
+      });
+      return res.status(200).json({ message: "Friend added" });
+    } catch (error) {
+      return res.status(500).json({ error: error });
+    }
+  return res.status(200).json({ incomplete: "Friend already exists" });
+}
 
 exports.app = functions.https.onRequest(app);
 
